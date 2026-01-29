3c6083dc0182fb6fe49fce46e6a95d3639df31f1
malak-majdoub <malak.majdoub@motopp.nl>
2026-01-29 15:44:09 +0100

    feat: validate data

--- schemas.py
+++ schemas.py
@@ -1,13 +1,14 @@
-from pydantic import BaseModel
+from typing import Literal
+from pydantic import BaseModel, Field, EmailStr
 class UserBase(BaseModel):
-    username: str
-    email: str
-    password: str
-    role: str
+    username: str = Field(..., min_length=1, max_length=50)
+    email: str = EmailStr
+    password: str = Field(..., min_length=8)
+    role: Literal["guest","hotel manager"]
 
 class UserDisplay(BaseModel):
     username: str
     email: str
     role: str
-    class Config():
+    class Config:
         orm_mode = True
