e14c020c9596d97013d11c462b562ad434ebe108
malak-majdoub <malak.majdoub@motopp.nl>
2026-01-29 13:30:05 +0100

    chore:create DB

---
+++ db/models.py
@@ -0,0 +1,8 @@
+from db.database import Base
+from sqlalchemy import Column
+from sqlalchemy.sql.sqltypes import Integer, String
+class DbUser(Base):
+    __tablename__ = "users"
+    id= Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    password = Column(String)
